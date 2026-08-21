3574722e1d23a7c4f8304065d4ae756193d7702f
drag <drag0n1zed@outlook.com>
2026-08-21 17:21:01 +0800

    change to fira code font

--- rotomdex-web/src/main.rs
+++ rotomdex-web/src/main.rs
@@ -60,9 +60,9 @@ async fn load_font() -> Result<()> {
         .and_then(|window| window.document())
         .ok_or_else(|| eyre!("unable to access the browser document"))?;
 
-    JsFuture::from(document.fonts().load_with_text("16px 'Cascadia Code NF'", "█"))
+    JsFuture::from(document.fonts().load_with_text("16px 'Fira Code'", "█"))
         .await
-        .map_err(|error| eyre!("unable to load Cascadia Code NF: {error:?}"))?;
+        .map_err(|error| eyre!("unable to load Fira Code: {error:?}"))?;
 
     Ok(())
 }
