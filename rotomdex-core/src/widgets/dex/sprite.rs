b2157258a2d42637b8c5fffec5cb23bd6ca475a7
drag <drag0n1zed@outlook.com>
2026-08-25 13:29:07 +0800

    use rust based chafa port for sprite rendering

--- rotomdex-core/src/widgets/dex/sprite.rs
+++ rotomdex-core/src/widgets/dex/sprite.rs
@@ -1,18 +1,7 @@
-use std::time::Duration;
+use std::{char, time::Duration};
 
-use image::{
-    Pixel, RgbaImage,
-    imageops::{self, FilterType},
-};
-use ratatui::{
-    prelude::*,
-    style::Color,
-    symbols::Marker,
-    widgets::{
-        Widget,
-        canvas::{Canvas, Painter, Shape},
-    },
-};
+use chafa_syms_rs::{Canvas, CanvasConfig, CanvasMode, PixelType};
+use ratatui::{prelude::*, widgets::Widget};
 
 use crate::model::ModelSprite;
 
@@ -29,6 +18,13 @@ impl<'a> SpriteWidget<'a> {
 
 impl<'a> Widget for SpriteWidget<'a> {
     fn render(self, area: Rect, buf: &mut Buffer) {
+        let side_width = area.width.min(area.height * 2);
+        let side_height = side_width / 2;
+        let area = area.inner(Margin::new(
+            (area.width - side_width) / 2,
+            (area.height - side_height) / 2,
+        ));
+
         let Some(sprite) = self
             .sprite
             .animated()
@@ -37,41 +33,39 @@ impl<'a> Widget for SpriteWidget<'a> {
         else {
             return; // TODO: handle missing sprite case. maybe render some kind of image that shows that there is no sprite
         };
-        let side_len = area.width.min(area.height.saturating_mul(2));
-        let side_len = side_len - side_len % 2;
-        if side_len < 2 {
-            return;
+
+        let cfg = CanvasConfig::new(area.width as usize, area.height as usize).mode(CanvasMode::Truecolor);
+        let mut canvas = Canvas::new(cfg);
+        canvas.draw_all_pixels(
+            PixelType::Rgba8,
+            sprite.as_raw(),
+            sprite.width() as usize,
+            sprite.height() as usize,
+            sprite.width() as usize * 4,
+        );
+
+        for (idx, source) in canvas.cells().iter().enumerate() {
+            let (fg_alpha, fg_color) = aarrggbb_to_color(source.fg);
+            let (bg_alpha, bg_color) = aarrggbb_to_color(source.bg);
+            let ch = char::from_u32(source.c).unwrap_or(' ');
+            let x = area.x + idx as u16 % area.width;
+            let y = area.y + idx as u16 / area.width;
+            if let Some(target) = buf.cell_mut((x, y)) {
+                match (fg_alpha < 128, bg_alpha < 128) {
+                    (true, true) => target.set_char(' '),
+                    (true, false) => target.set_char(ch).set_fg(bg_color).set_style(Modifier::REVERSED),
+                    (false, true) => target.set_char(ch).set_fg(fg_color),
+                    (false, false) => target.set_char(ch).set_fg(fg_color).set_bg(bg_color),
+                };
+            }
         }
-
-        let area = area.centered(Constraint::Length(side_len), Constraint::Length(side_len / 2));
-        let sprite: RgbaImage = imageops::resize(sprite, side_len.into(), side_len.into(), FilterType::Nearest);
-        let max = (side_len - 1) as f64;
-
-        Canvas::default()
-            .x_bounds([0.0, max])
-            .y_bounds([0.0, max])
-            .marker(Marker::HalfBlock)
-            .paint(|ctx| {
-                ctx.draw(&RotomDexSprite(&sprite));
-            })
-            .render(area, buf);
     }
 }
 
-struct RotomDexSprite<'a>(&'a RgbaImage);
-
-impl<'a> Shape for RotomDexSprite<'a> {
-    fn draw(&self, painter: &mut Painter) {
-        let height = self.0.height();
-
-        for (x, y, color) in self.0.enumerate_pixels() {
-            if color.alpha() == 0 {
-                continue;
-            }
-            if let Some((x, y)) = painter.get_point(x.into(), (height - y - 1).into()) {
-                let color = Color::Rgb(color.0[0], color.0[1], color.0[2]);
-                painter.paint(x, y, color);
-            }
-        }
-    }
+fn aarrggbb_to_color(color: u32) -> (u8, Color) {
+    let a = (color >> 24) as u8;
+    let r = (color >> 16) as u8;
+    let g = (color >> 8) as u8;
+    let b = color as u8;
+    (a, Color::Rgb(r, g, b))
 }
